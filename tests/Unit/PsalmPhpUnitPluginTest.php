1488b5c419b9f62be690f46470563c16cd65576d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-07-20 03:03:36 -0500

    Update PsalmPhpUnitPluginTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/PsalmPhpUnitPluginTest.php
+++ tests/Unit/PsalmPhpUnitPluginTest.php
@@ -5,14 +5,33 @@ declare(strict_types=1);
 namespace Ghostwriter\PsalmPluginTester\Tests\Unit;
 
 use Generator;
-use Ghostwriter\Json\Json;
+use Ghostwriter\PsalmPluginTester\Path\Directory\DirectoryTrait;
 use Ghostwriter\PsalmPluginTester\Path\Directory\Fixture;
+use Ghostwriter\PsalmPluginTester\Path\Directory\ProjectRootDirectory;
+use Ghostwriter\PsalmPluginTester\Path\File\ExpectationsJsonFile;
+use Ghostwriter\PsalmPluginTester\Path\File\FileTrait;
+use Ghostwriter\PsalmPluginTester\Path\PathTrait;
 use Ghostwriter\PsalmPluginTester\PluginTester;
+use Ghostwriter\PsalmPluginTester\PluginTestResult;
+use Ghostwriter\PsalmPluginTester\Shell;
+use Ghostwriter\PsalmPluginTester\ShellResult;
+use Ghostwriter\PsalmPluginTester\Value\Expectation;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Psalm\PhpUnitPlugin\Plugin;
 
 #[CoversClass(PluginTester::class)]
+#[CoversClass(Fixture::class)]
+#[CoversClass(ExpectationsJsonFile::class)]
+#[CoversClass(ProjectRootDirectory::class)]
+#[CoversClass(PluginTester::class)]
+#[CoversClass(PluginTestResult::class)]
+#[CoversClass(Shell::class)]
+#[CoversClass(ShellResult::class)]
+#[CoversClass(Expectation::class)]
+#[CoversClass(FileTrait::class)]
+#[CoversClass(PathTrait::class)]
+#[CoversClass(DirectoryTrait::class)]
 final class PsalmPhpUnitPluginTest extends TestCase
 {
     private PluginTester $pluginTester;
@@ -32,6 +51,10 @@ final class PsalmPhpUnitPluginTest extends TestCase
     /** @dataProvider fixtureDataProvider */
     public function testPlugin(Fixture $fixture): void
     {
-        $this->pluginTester->test($fixture);
+        $result = $this->pluginTester->test($fixture);
+
+        $result->assertExitCode(2);
+
+        self::assertSame($fixture, $result->getFixture());
     }
 }
