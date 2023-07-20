909d8e943a5c90c54b4417897bf05f972409ffc6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-07-20 01:32:16 -0500

    Create ExpectationsJsonFile.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Path/File/ExpectationsJsonFile.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\PsalmPluginTester\Path\File;
+
+use Ghostwriter\Json\Json;
+use Ghostwriter\PsalmPluginTester\Value\Expectation;
+use RuntimeException;
+
+final class ExpectationsJsonFile implements FileInterface
+{
+    use FileTrait;
+
+    public function __construct(
+        private readonly string $path,
+    ) {
+    }
+
+    public function getExpectations(): array
+    {
+        if (! $this->exists()) {
+            return ['null'];
+        }
+
+        $expectations = $this->read();
+
+        return array_map(
+            static fn (
+                array $expectation
+            ): Expectation => new Expectation(
+                $expectation['file'],
+                $expectation['type'],
+                $expectation['message']
+            ),
+            $expectations['error'] ?? [],
+            $expectations['warning'] ?? []
+        );
+    }
+
+    /**
+     * @return array{'errors': array{'file': string, 'type': string, 'message': string}}
+     */
+    private function read(): array
+    {
+        $contents = file_get_contents($this->path);
+
+        if ($contents === false) {
+            throw new RuntimeException(sprintf('Could not read expectation file: "%s"', $this->path));
+        }
+
+        return Json::decode($contents);
+    }
+}
