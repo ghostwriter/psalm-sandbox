5f607cf4ab3fcf4ffd156977a48a536cf24fb356
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-07-31 17:57:00 -0500

    Update FileTrait.php

--- src/Path/File/FileTrait.php
+++ src/Path/File/FileTrait.php
@@ -10,14 +10,6 @@ trait FileTrait
 {
     use PathTrait;
 
-    public function __construct(
-        private readonly string $path,
-    ) {
-        if (! is_file($this->path)) {
-            throw new RuntimeException(sprintf('File "%s" does not exist', $this->file));
-        }
-    }
-
     public function getFile(): string
     {
         return $this->path;
