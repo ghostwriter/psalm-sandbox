2260dea7af40d3f881a66d1fe3ff25a55ce923d7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-01 13:39:03 -0500

    Create ExpectationTest.php

---
+++ tests/Unit/ExpectationTest.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\PsalmPluginTester\Tests\Feature;
+
+use Ghostwriter\PsalmPluginTester\Expectation;
+use JsonSerializable;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Stringable;
+
+#[CoversClass(Expectation::class)]
+final class ExpectationTest extends TestCase
+{
+    private Expectation $expectation;
+
+    private string $file;
+
+    private string $message;
+
+    private string $severity;
+
+    private string $type;
+
+    protected function setUp(): void
+    {
+        $this->file = __FILE__;
+        $this->message = 'Black Lives Matter';
+        $this->severity = 'notice';
+        $this->type = 'black-lives-matter';
+
+        $this->expectation = new Expectation(
+            $this->file,
+            $this->message,
+            $this->severity,
+            $this->type,
+        );
+    }
+
+    public function testExpectation(): void
+    {
+        Assert::assertInstanceOf(
+            JsonSerializable::class,
+            $this->expectation
+        );
+
+        Assert::assertInstanceOf(
+            Stringable::class,
+            $this->expectation
+        );
+    }
+
+    public function testFile(): void
+    {
+        Assert::assertSame($this->file, $this->expectation->getFile());
+    }
+
+    public function testJsonSerialize(): void
+    {
+        Assert::assertSame(
+            $this->file,
+            $this->expectation->jsonSerialize()['file']
+        );
+
+        Assert::assertSame(
+            $this->message,
+            $this->expectation->jsonSerialize()['message']
+        );
+
+        Assert::assertSame(
+            $this->severity,
+            $this->expectation->jsonSerialize()['severity']
+        );
+
+        Assert::assertSame(
+            $this->type,
+            $this->expectation->jsonSerialize()['type']
+        );
+    }
+
+    public function testMessage(): void
+    {
+        Assert::assertSame($this->message, $this->expectation->getMessage());
+    }
+
+    public function testSeverity(): void
+    {
+        Assert::assertSame($this->severity, $this->expectation->getSeverity());
+    }
+
+    public function testType(): void
+    {
+        Assert::assertSame($this->type, $this->expectation->getType());
+    }
+}
