ca6f070be57be8f403abc3a12766bbec532a9681
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-06-16 13:36:33 -0500

    Update `bootstrap.php` file
    Updates the `bootstrap.php` file to include the autoloader and memory limit settings.
    
    This pull request changes the following file(s):
    
    - Update `tests/bootstrap.php`
    
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,9 +12,14 @@ if (! $classLoader instanceof ClassLoader) {
     throw new \RuntimeException('Class loader not found');
 }
 
+ini_set('memory_limit', '-1');
+
 $path = __DIR__ . \DIRECTORY_SEPARATOR . 'Fixture';
 
 if (\is_dir($path)) {
+    /** @psalm-suppress UncaughtThrowInGlobalScope */
+    $classLoader->addPsr4('', $path);
+
     /** @psalm-suppress UncaughtThrowInGlobalScope */
     $classLoader->addPsr4('Tests\\Fixture\\', $path);
 }
